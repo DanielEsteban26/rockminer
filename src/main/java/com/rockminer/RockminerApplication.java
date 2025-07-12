b14167809913fc62fb86a2bf9ba8813b09e23b27
Daniel Isidro <isidroesteban402@gmail.com>
2025-07-12 17:16:30 -0500

    Primer commit - Backend Spring Boot

---
+++ src/main/java/com/rockminer/RockminerApplication.java
@@ -0,0 +1,13 @@
+package com.rockminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockminerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockminerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rockminer/RockminerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockminer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockminerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
